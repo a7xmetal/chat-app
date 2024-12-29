0c91af64b04919cf69a4f61d928f3f48b9329462
Niraj <love.cof666@gmail.com>
2024-12-29 14:45:42 +0545

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,6 +12,8 @@ import connectDB from "./db/db.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5050;
+
+const __dirname = path.resolve(); //returns the absolute path of the current directory
 
 dotenv.config();
 
@@ -29,6 +32,14 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Serve the index.html file for all routes not defined above (SPA routing)
+// This allows client-side JavaScript to handle rendering for routes like /home, /about, etc.
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.get("/", (req, res) => {
    res.send("Hello World!");
